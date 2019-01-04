7d95c90aaf7a9aaadf0752aded2f95e53d60976d
sxlsjf <771475507@qq.com>
2019-01-04 14:51:44 +0800

    Initial commit Ant

---
+++ src/java/Test.java
@@ -0,0 +1,12 @@
+package java;
+
+/**
+ * @Author: shenxl
+ * @Date: 2019/1/4 13:25
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hellword ");
+    }
+}
